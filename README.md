36a40f9147e5bb5bb92fecd454dc903a817be6c3
zhaolei-hu <zhaolei_hu@foxmail.com>
2024-07-15 08:36:11 +0800

    feat: add dep

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Super Survey
 问卷调查
 Nextjs全栈开发
+.env文件已加入到.gitignore，保留了示例文件.env.example,自行配置数据库信息
---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,56 @@
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils"
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+import '../globals.css'
+
+type Props = {
+  params: {locale: string}
+  searchParams: {}
+}
+export function generateMetadata({params}:  Props) {
+  if (params.locale === 'zh') {
+    return {
+      title: '超级问卷',
+      description: '超级问卷...'
+    }
+  }
+  return {
+    title: "Super Survey",
+    description: "Super Survey...",
+  }
+}
+
+export default async function LocaleLayout({
+  children,
+  params: {locale}
+}: {
+  children: React.ReactNode;
+  params: {locale: string};
+}) {
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+ 
+  return (
+    <html lang={locale}>
+      <head>
+        <link rel="icon" href="/icon.ico" />
+      </head>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
