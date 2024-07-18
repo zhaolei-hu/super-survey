29586edf6a7867c83dfd4b7e7b2aec779221a75b
zhaolei-hu <zhaolei_hu@foxmail.com>
2024-07-18 10:05:18 +0800

    feat: add eslint prettier rules

--- db/prisma.ts
+++ db/prisma.ts
@@ -3,8 +3,8 @@ import { PrismaClient } from '@prisma/client'
 const prismaClientSingleton = () => {
   return new PrismaClient()
 }
-
 declare global {
+  // eslint-disable-next-line no-unused-vars
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
